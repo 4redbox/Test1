54719b461e6a5cddd12ae2a3a1f99551b1e94f01
Saravanesh <raja4redbox@gmail.com>
2017-03-01 01:18:51 -0500

    Commit

---
+++ Test1.py
@@ -0,0 +1,3 @@
+print("Welcome to slver studios")
+username = raw_input("We need a funny name to address you: ")
+print('Aww... you ' + username + ' ')
---
+++ Test2.py
@@ -0,0 +1,20 @@
+"""program to guess number"""
+import random
+print("Welcome to slver studios")
+username = raw_input("We need a funny name to address you: ")
+print('Aww...' + username + ', welcome')
+rnum = random.randint(1, 100)
+#print('rnum: ' + str(rnum))
+gotit = False
+
+while not gotit:
+    num = raw_input(username + " guess a number: ")
+    num = int(num)
+
+    if num == rnum:
+        print(str(num) + ' is correct, ' + username)
+        gotit = True
+    elif num > rnum:
+        print(username + ' you guessed high')
+    else:
+        print(username + ' you guessed low')
